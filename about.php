ace1340e5259c09e91d36f1e3bd22fcab4286994
madmanham11 <smithphilip812@gmail.com>
2023-05-14 17:05:24 -0600

    adding something to the about and contact pages for a rough outline

--- about.php
+++ about.php
@@ -1,6 +1,13 @@
 <?php
 include_once('header.php');
 ?>
+<body>
+    <h1>About Us</h1>
+</body>
+<br/>
+<body>
+    <p>our goal is to make a cms that will allow our users to expand there horizons </p>
+</body>
 <br/>
 <?php
 include_once('footer.php');
--- contact.php
+++ contact.php
@@ -1,6 +1,17 @@
 <?php
 include_once('header.php');
 ?>
+<body>
+    <h1>Contact us!</h1>
+</body>
+<br />
+<body>
+    <p>If you have any questions or concerns we would like to hear you out and resolve and errors you may encounter</p>
+    <br/>
+    <p>Phone: 111-222-3333</p>
+    <br />
+    <p>Email: nothingSpecial@me.com</p>
+</body>
 <br />
 <?php
 include_once('footer.php');
