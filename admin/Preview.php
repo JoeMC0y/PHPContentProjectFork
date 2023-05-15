8d342ec1e4d88174a136ca4aa8da19579eaf34e3
madmanham11 <smithphilip812@gmail.com>
2023-05-14 23:06:20 -0600

    fix

--- admin/Preview.php
+++ admin/Preview.php
@@ -1,3 +1,9 @@
 <?php
-
+include_once('header.php');
+include_once('addpage.php');
+echo $htmlContent;
+?>
+<br />
+<?php
+include_once('footer.php');
 ?>
--- admin/addpage.php
+++ admin/addpage.php
@@ -3,6 +3,25 @@ include_once('header.php');
 $htmlContent = null;
 ?>
 <br />
+<table>
+    <tr>
+        <td>
+            <button name="h1"></button>
+        </td>
+    
+        <td>
+            <button name="h2"></button>
+        </td>
+   
+        <td>
+            <button name="h3"></button>
+        </td>
+    
+        <td>
+            <button name="Paragraph"></button>
+        </td>
+    </tr>
+</table>
 <?php
 include_once('footer.php');
 ?>
