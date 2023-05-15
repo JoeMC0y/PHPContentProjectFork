c18e0c50cbade1bed7e4c3f6863dc63c3d86ead6
madmanham11 <smithphilip812@gmail.com>
2023-05-15 16:25:36 -0600

    added button

--- account/login.php
+++ account/login.php
@@ -5,6 +5,7 @@ Username : &nbsp;
 <input type="text" id="username" value="1" />
 password : &nbsp;
 <input type="text" id="password" value="1"/>
+<button name="a" onclick="myClickEvent()">Submit</button>
 <br />
 <?php
 include_once('footer.php');
