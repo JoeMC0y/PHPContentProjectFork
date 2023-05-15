5e00e34e079bc9665070b8b1a8e7e79cc3c413cb
ethcala <81577564+ethcala@users.noreply.github.com>
2023-05-15 11:24:33 -0600

    most of add page

--- about/history.php
+++ about/history.php
@@ -2,6 +2,8 @@
 include_once('header.php');
 ?>
 <br />
+<h1>History</h1>
+<p>We are a great company who has been active for approximately 2 million years. We pride ourselves on our beautiful products of all shapes and sizes, humans.</p>
 <?php
 include_once('footer.php');
 ?>
--- about/mission.php
+++ about/mission.php
@@ -2,6 +2,8 @@
 include_once('header.php');
 ?>
 <br />
+<h1>Mission</h1>
+<p>Simply put, our mission is the evolution and success of mankind. We have been working towards this goal for a long time, and we hope that our passion for this project shines through in our work.</p>
 <?php
 include_once('footer.php');
 ?>
