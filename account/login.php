e634f69aff888f7c028b7d20b6fd6a2668365b2b
madmanham11 <smithphilip812@gmail.com>
2023-05-15 16:24:13 -0600

    created a rough outline of what we should make out user login have

--- account/login.php
+++ account/login.php
@@ -1,6 +1,10 @@
 <?php
 include_once('header.php');
 ?>
+Username : &nbsp;
+<input type="text" id="username" value="1" />
+password : &nbsp;
+<input type="text" id="password" value="1"/>
 <br />
 <?php
 include_once('footer.php');
