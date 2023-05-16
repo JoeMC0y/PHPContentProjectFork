8b8e276d4acd00f2ec5e2ac924f639c9a6d4b91e
madmanham11 <smithphilip812@gmail.com>
2023-05-15 21:07:14 -0600

    little more simple changes

--- about/mission.php
+++ about/mission.php
@@ -2,8 +2,11 @@
 include_once('header.php');
 ?>
 <br />
+<body>
 <h1>Mission</h1>
 <p>Simply put, our mission is the evolution and success of mankind. We have been working towards this goal for a long time, and we hope that our passion for this project shines through in our work.</p>
+    </body>
+<br/>
 <?php
 include_once('footer.php');
 ?>
--- contact/email.php
+++ contact/email.php
@@ -1,6 +1,7 @@
 <?php
 include_once('header.php');
 ?>
+<br/>
 <body>
 <h1>Email</h1>
 <p>if you ever need our support please feel free to reach out to us. you can reach us at busy@me.com</p>
