69a8b9f75661f8f8ec3fa0d40af7a184940461e1
JoeMC0y <80136727+JoeMC0y@users.noreply.github.com>
2023-05-16 16:21:21 -0600

    Logout now deletes the cookies and resets index

--- account/logout.php
+++ account/logout.php
@@ -1,3 +1,12 @@
 <?php
 
+
+if (isset($_COOKIE["user"])) {
+    setcookie("user", "", time() - 100000, "/");
+    setcookie("password", "", time() - 100000, "/");
+    header("Location: http://localhost:5435/");
+    exit();
+}
+
+
 ?>
