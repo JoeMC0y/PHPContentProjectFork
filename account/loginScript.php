9b90876a8f2ed706cd95cc3c07c3cd6e5d121b6b
JoeMC0y <80136727+JoeMC0y@users.noreply.github.com>
2023-05-17 08:49:15 -0600

    login connected to db
    login now works with db I need to specify the directory for include dbConnect.php

--- account/loginScript.php
+++ account/loginScript.php
@@ -1,19 +1,30 @@
 <?php
 
-
-DEFINE('Username', 'JohnDoe');
-DEFINE('Password', 'passwurd');
+include 'dbConnect.php';
 
 
-if($_POST["name"] == Username && $_POST["pass"] == Password){
-    setcookie("user", Username, time() + (86400 * 30), "/");
-    setcookie("password", Password, time() + (86400 * 30), "/");
-    setcookie("style", "\default.css", time() + (86400 * 30), "/");
-    header("Location: http://localhost:5435/");
-    exit();
-}else{
-    echo "no match";
-}
+$connec = ConnGet();
+$users = getUser($connec, $_POST["name"], $_POST["pass"]);
+
+
+    if ($users->num_rows > 0) {
+        // output data of each row
+        while($row = $users->fetch_assoc()) {
+
+            $user = $row["username"];
+            $pas = $row["password"];
+            setcookie("user", $user, time() + (86400 * 30), "/");
+            setcookie("password", $pas, time() + (86400 * 30), "/");
+            setcookie("style", "\default.css", time() + (86400 * 30), "/");
+            header("Location: http://localhost:5435/");
+            exit();
+
+        }
+    } else {
+        echo "No Matches";
+    }
+    
+
 
 
 
--- data/dbConnect.php
+++ data/dbConnect.php
@@ -1,8 +1,8 @@
 <?php
 DEFINE('DB_USER','root');
-DEFINE('DB_PSWD', 'P@r7y');
-define('DB_SERVER', 'localhost');
-define('DB_NAME', 'table1');
+DEFINE('DB_PSWD', 'War3Fram~');
+define('DB_SERVER', '127.0.0.1:3306');
+define('DB_NAME', 'user');
 
 function ConnGet(){
     $dbConn = @mysqli_connect(DB_SERVER,DB_USER,DB_PSWD,DB_NAME)
@@ -16,22 +16,25 @@ function ConnGet(){
 //select * from new_table where username = 'mad' and password = '123';
 
 function getUser($dbConn,$username,$password){
-    $query = "select * from new_table where username =".$username." and password = ".$password.";";
-    return @mysqli_query($dbConn,$query);
+    $query = "select * from `users` where `username` = '" . $username . "' and `password` = '" . $password ."'" ;
+    return @mysqli_query($dbConn, $query);
 }
 
 function addUsers($dbConn,$username,$password){
-    $query ="insert into new_table (userId ,username, password, isAdmin)values(2,".$username.",".$password.", false);";
+    $query1 = "select * from users where username =".$username.";";
+
+
+    $query ="insert into users (username, password, isAdmin)values(".$username.",".$password.", false);";
     return @mysqli_query($dbConn,$query);
 }
 
 function getAdmin($dbConn,$username,$password){
-    $query = "select * from new_table where username =".$username." and password = ".$password."and isAdmin = true;";
+    $query = "select * from users where username =".$username." and password = ".$password."and isAdmin = true;";
     return @mysqli_query($dbConn,$query);
 }
 
 function addAdmin($dbConn,$username,$password){
-    $query ="insert into new_table (userId ,username, password, isAdmin)values(2,".$username.",".$password.", true);";
+    $query ="insert into users (username, password, isAdmin)values(".$username.",".$password.", true);";
     return @mysqli_query($dbConn,$query);
 }
 ?>
